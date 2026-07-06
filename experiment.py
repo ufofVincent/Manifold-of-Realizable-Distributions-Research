94fec28a2f3104fabb926e91ec126a1ac03742a8
Vincent Han <vhan@bowdoin.edu>
2026-07-06 16:14:56 -0700

    new

--- experiment.py
+++ experiment.py
@@ -1,7 +1,9 @@
 import torch 
 import torch.nn as nn 
+import torch.optim as optim
+import copy
 
-class Generator(torch.Module):
+class DistributionGenerator(torch.Module):
     def __init__(self, input_dim, output_dim, hidden_layer_num, hidden_layer_dim):
         super(self).__init__()
         
@@ -10,6 +12,9 @@ class Generator(torch.Module):
         self.layers.append(nn.linear(input_dim, hidden_layer_dim))
         
         self.layer_num = hidden_layer_num + 2
+        
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         
         for _ in range(hidden_layer_num):
             self.layers.append(nn.linear(hidden_layer_dim, hidden_layer_dim))
@@ -23,5 +28,37 @@ class Generator(torch.Module):
             input = self.layers[_](input)
             
         return input
+
+
+
+def compute_drift(sample: torch.tensor, generator: DistributionGenerator, kernel_function, sample_num: int):
     
+    #x = generator(sample)
     
+    #return kernel_function(x, ) * kernel_function(x, )
+    
+    pass 
+    
+def iterate(model: DistributionGenerator, sample_num: int, drift_field, kernel_function):
+    
+    old_model = copy.deepcopy(model)
+    
+    for _ in range(sample_num):
+        sample = torch.randn(model.input_dim)
+        
+        with torch.no_grad:
+            frozen_target = model(sample) + drift_field(sample, model, kernel_function, 100)
+        
+        sum += torch.linalg.norm(model(sample) - frozen_target)
+        
+    loss = sum / sample_num
+    
+    optimizer = optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-2)
+
+    optimizer.zero_grad()
+    
+    loss.backward()
+    
+    optimizer.step()
+    
+    return model, old_model
